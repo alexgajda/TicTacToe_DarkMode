e919fd5e54ba2ceb5a5116b1957ccd32abdd3b05
alexg <alexgajda13@gmail.com>
2021-08-21 19:10:50 +0300

    fixed giveNamesActivity (not showing)

--- app/src/main/java/com/example/tictactoewithdarkmode/gameActivity.java
+++ app/src/main/java/com/example/tictactoewithdarkmode/gameActivity.java
@@ -1,8 +1,14 @@
 package com.example.tictactoewithdarkmode;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class gameActivity extends AppCompatActivity {
 
@@ -11,7 +17,15 @@ public class gameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        ImageButton returntomain = (ImageButton) findViewById(R.id.button_exit);
+        returntomain.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
+        });
 
+        giveNamesActivity names = new giveNamesActivity();
+        TextView player1_text = findViewById(R.id.playerX_text);
 
     }
 }
--- app/src/main/java/com/example/tictactoewithdarkmode/giveNamesActivity.java
+++ app/src/main/java/com/example/tictactoewithdarkmode/giveNamesActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class giveNamesActivity extends AppCompatActivity {
+
+    String playerX, playerO;
+
+    EditText playerX_input;
+    EditText playerO_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,36 @@ public class giveNamesActivity extends AppCompatActivity {
             overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
         });
 
-        
+    }
+
+    public void handleText(View v) {
+        playerX_input = (EditText) findViewById(R.id.playerX_input);
+        playerO_input = (EditText) findViewById(R.id.playerO_input);
+
+        playerX = playerX_input.getText().toString();
+        playerO = playerO_input.getText().toString();
+
+        if (playerX.equals("")) {
+            playerX = "Player X";
+        }
+        if (playerO.equals("")) {
+            playerO = "Player O";
+        }
+
+        Intent intent = new Intent(giveNamesActivity.this, gameActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
+
+        Log.d("playerX:", playerX);
+        Log.d("playerO:", playerO);
+
+    }
+
+    public String getPlayerX() {
+        return playerX;
+    }
+
+    public String getPlayerO() {
+        return playerO;
     }
 }
