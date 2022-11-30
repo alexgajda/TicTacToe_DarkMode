0be9b9630cbdba04c305e9d6b9fd7ea5517cb67c
alexg <alexgajda13@gmail.com>
2022-12-01 00:23:30 +0200

    x and o to buttons

--- app/src/main/java/com/example/tictactoewithdarkmode/gameActivity.java
+++ app/src/main/java/com/example/tictactoewithdarkmode/gameActivity.java
@@ -7,10 +7,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class gameActivity extends AppCompatActivity {
+
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +42,148 @@ public class gameActivity extends AppCompatActivity {
 
         //Scoreboard
         //////initialize
-        String scoreX = "0";
-        String scoreO = "0";
+        int scoreX = 0;
+        int scoreO = 0;
 
         TextView sX = findViewById(R.id.playerXscore);
         TextView sY = findViewById(R.id.playerO_score);
 
-        sX.setText(scoreX);
-        sY.setText(scoreO);
+
+        sX.setText(String.valueOf(scoreX));
+        sY.setText(String.valueOf(scoreO));
+
+        Button tl = findViewById(R.id.buttonTL);
+        Button tm = findViewById(R.id.buttonTM);
+        Button tr = findViewById(R.id.buttonTR);
+        Button ml = findViewById(R.id.buttonML);
+        Button mm = findViewById(R.id.buttonMM);
+        Button mr = findViewById(R.id.buttonMR);
+        Button bl = findViewById(R.id.buttonBL);
+        Button bm = findViewById(R.id.buttonBM);
+        Button br = findViewById(R.id.buttonBR);
 
 
-    }
+
+
+            tl.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        tl.setText("X");
+                    }else{
+                        tl.setText("O");
+                    }
+                    tl.setClickable(false);
+                    num++;
+                }
+            });
+
+            tm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        tm.setText("X");
+                    }else{
+                        tm.setText("O");
+                    }
+                    tm.setClickable(false);
+                    num++;
+                }
+            });
+
+            tr.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        tr.setText("X");
+                    }else{
+                        tr.setText("O");
+                    }
+                    tr.setClickable(false);
+                    num++;
+                }
+            });
+
+            ml.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        ml.setText("X");
+                    }else{
+                        ml.setText("O");
+                    }
+                    ml.setClickable(false);
+                    num++;
+                }
+            });
+
+            mm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        mm.setText("X");
+                    }else{
+                        mm.setText("O");
+                    }
+                    mm.setClickable(false);
+                    num++;
+                }
+            });
+
+            mr.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        mr.setText("X");
+                    }else{
+                        mr.setText("O");
+                    }
+                    mr.setClickable(false);
+                    num++;
+                }
+            });
+
+            bl.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        bl.setText("X");
+                    }else{
+                        bl.setText("O");
+                    }
+                    bl.setClickable(false);
+                    num++;
+                }
+            });
+
+            bm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        bm.setText("X");
+                    }else{
+                        bm.setText("O");
+                    }
+                    bm.setClickable(false);
+                    num++;
+                }
+            });
+
+            br.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if ((num % 2) == 0){
+                        br.setText("X");
+                    }else{
+                        br.setText("O");
+                    }
+                    br.setClickable(false);
+                    num++;
+                }
+            });
+
+
+
+
+        }
 }
